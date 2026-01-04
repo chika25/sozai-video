5f6876ab6f5c25887b9254b3087f66c4d5540a00
Chika Kurihara <chikakurihara1993@gmail.com>
2026-01-03 23:52:47 -0500

    added contact forms

--- page-templates/template-contact.php
+++ page-templates/template-contact.php
@@ -12,17 +12,17 @@ get_header(); ?>
         <div class="main-wrapper">
             <?php get_template_part('template-parts/content-breadcrumbs'); ?>
             
-            <div class="form-container">
+            <div class="page-content">
                 <?php while ( have_posts() ) : the_post(); ?>
-                    <h1><?php the_title(); ?></h1>
+                    <h1 class="entry-title"><?php the_title(); ?></h1>
 
                     <?php echo do_shortcode('[contact-form-7 id="da20f51" title="Contact form 1"]'); ?>
                     
                     <?php if ( get_the_title() == '素材リクエスト' ) : ?>
-                        <p>素材リクエストありがとうございます。<br>
+                        <p class="contact-text">素材リクエストありがとうございます。<br>
                         リクエストへの個別返信は行っておりませんが、制作の参考にさせていただきます。</p>
                     <?php else : ?>
-                        <p>お問い合わせありがとうございます。当サイトに関するご意見はすべて確認させていただいております。<br>
+                        <p class="contact-text">お問い合わせありがとうございます。当サイトに関するご意見はすべて確認させていただいております。<br>
                         個別のご返信にはお時間をいただく場合がございますが、ご了承ください。
                         </p>
                     <?php endif; ?>
